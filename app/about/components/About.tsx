bc5baeba64983df27f9aab25aec03bcf542f5fe9
roast <roastt1@naver.com>
2025-09-23 23:49:21 +0900

    [feat] About 페이지에 AOS 스크롤 애니메이션 적용

--- app/about/components/About.tsx
+++ app/about/components/About.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Image from "next/image";
 import AboutCard from "./AboutCard";
 
 export default function About() {
+    useEffect(() => {
+        AOS.init({ duration: 800 });
+    }, []);
+
     return (
         <div className="flex w-full flex-col items-center pt-8">
-            <div className="mb-12 flex flex-col items-center gap-10 text-center lg:flex-row">
+            <div className="mb-12 flex flex-col items-center gap-10 text-center">
                 {/* 프로필 사진 */}
-                <div className="relative mb-4 h-72 w-72 animate-slide-up overflow-hidden rounded-3xl border-4 border-blue-400 shadow-md lg:h-96 lg:w-96">
+                <div
+                    className="relative mb-4 h-96 w-72 animate-slide-up overflow-hidden rounded-3xl border-4 border-blue-400 shadow-md sm:h-[450px] sm:w-[400px]"
+                    data-aos="fade-up"
+                    data-aos-delay={100}
+                >
                     <Image
                         src="/images/about/about_me.jpg"
                         alt="Profile Picture"
@@ -18,69 +30,68 @@ export default function About() {
                 </div>
 
                 {/* 소개 */}
-                <div className="mb-4 flex animate-slide-up flex-col items-center space-y-2 whitespace-nowrap lg:items-start">
+                <div className="mb-4 flex animate-slide-up flex-col items-center space-y-2 whitespace-nowrap">
                     <h1
-                        className="mb-4 animate-slide-up text-xl font-bold sm:text-3xl lg:text-4xl"
-                        style={{ animationDelay: "0.2s" }}
+                        className="mb-4 animate-slide-up text-2xl font-bold sm:text-3xl"
+                        data-aos="fade-up"
+                        data-aos-delay={200}
                     >
                         안녕하세요! 박준선입니다.
                     </h1>
                     <p
-                        className="max-w-xl animate-slide-up text-sm sm:text-xl lg:text-2xl"
-                        style={{ animationDelay: "0.4s" }}
+                        className="max-w-xl animate-slide-up text-sm sm:text-xl"
+                        data-aos="fade-up"
+                        data-aos-delay={400}
                     >
                         항상 사용자 입장에서 고민하며,
                     </p>
                     <p
-                        className="max-w-xl animate-slide-up text-sm sm:text-xl lg:text-2xl"
-                        style={{ animationDelay: "0.7s" }}
+                        className="max-w-xl animate-slide-up text-sm sm:text-xl"
+                        data-aos="fade-up"
+                        data-aos-delay={700}
                     >
                         더 나은 경험을 만드는 프론트엔드 개발자입니다.
                     </p>
                     <p
-                        className="max-w-2xl animate-slide-up text-sm sm:text-xl lg:text-2xl"
-                        style={{ animationDelay: "1s" }}
+                        className="max-w-2xl animate-slide-up text-sm sm:text-xl"
+                        data-aos="fade-up"
+                        data-aos-delay={1000}
                     >
                         누구나 직관적이고 편하게 사용할 수 있는 웹을 지향합니다.
                     </p>
                 </div>
             </div>
+
             {/* 정보 카드 */}
-            <div className="mx-auto grid w-full grid-cols-1 justify-items-center gap-x-4 gap-y-6 md:grid-cols-2 lg:grid-cols-3">
+            <div className="mx-auto grid w-full grid-cols-1 justify-items-center gap-x-4 gap-y-12 sm:grid-cols-2 sm:gap-y-16">
                 <AboutCard
                     title="이름"
                     description="박준선"
-                    animationDelay="0.2s"
                     iconSrc="/icons/about/name.svg"
                 />
                 <AboutCard
                     title="생년월일"
                     description="2000.02.18"
-                    animationDelay="0.3s"
                     iconSrc="/icons/about/birth.svg"
                 />
                 <AboutCard
                     title="위치"
                     description="경기도 남양주시"
-                    animationDelay="0.4s"
                     iconSrc="/icons/about/location.svg"
                 />
                 <AboutCard
                     title="연락처"
                     description="010-8298-9208"
-                    animationDelay="0.5s"
                     iconSrc="/icons/about/contact.svg"
                 />
                 <AboutCard
                     title="이메일"
                     description="roastt1@naver.com"
-                    animationDelay="0.6s"
                     iconSrc="/icons/about/email.svg"
                 />
                 <AboutCard
                     title="학력"
                     description="서경대학교 소프트웨어학과"
-                    animationDelay="0.7s"
                     iconSrc="/icons/about/education.svg"
                 />
             </div>
--- app/about/components/AboutCard.tsx
+++ app/about/components/AboutCard.tsx
@@ -5,25 +5,24 @@ import Image from "next/image";
 interface LandingCardProps {
     title: string;
     description: string;
-    animationDelay?: string;
     iconSrc?: string;
 }
 
 export default function AboutCard({
     title,
     description,
-    animationDelay = "0s",
     iconSrc,
 }: LandingCardProps) {
     return (
         <div
-            className="h-[160px] w-full max-w-[240px] animate-slide-up lg:h-[180px] lg:max-w-[340px]"
-            style={{ animationDelay }}
+            className="h-[160px] w-full max-w-[280px] sm:h-[180px] sm:max-w-[500px]"
+            data-aos="fade-up"
+            data-aos-delay="200"
         >
             <div className="flex h-full flex-col items-center justify-center rounded-2xl border-2 border-blue-100 bg-white/90 px-6 py-6 text-center shadow-lg backdrop-blur-sm transition-all duration-300 ease-out hover:-translate-y-2 hover:shadow-2xl">
                 {/* 아이콘 */}
                 {iconSrc && (
-                    <div className="mb-2 flex h-12 w-12 items-center justify-center rounded-xl bg-blue-200 shadow-lg lg:mb-3 lg:h-14 lg:w-14">
+                    <div className="mb-2 flex h-12 w-12 items-center justify-center rounded-xl bg-blue-200 shadow-lg sm:mb-4 sm:h-14 sm:w-14">
                         <Image
                             src={iconSrc}
                             alt="아이콘"
@@ -38,10 +37,10 @@ export default function AboutCard({
                 )}
 
                 {/* 텍스트 */}
-                <h2 className="mb-1 text-lg font-semibold text-gray-800 lg:text-xl">
+                <h2 className="mb-1 text-lg font-semibold text-gray-800 sm:text-xl">
                     {title}
                 </h2>
-                <p className="text-sm leading-relaxed text-gray-600 lg:text-base">
+                <p className="text-sm leading-relaxed text-gray-600 sm:text-base">
                     {description}
                 </p>
             </div>
