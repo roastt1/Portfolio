279d49fd93702e73160910aac654466101c3229e
roast <roastt1@naver.com>
2026-01-06 15:25:22 +0900

    [refactor] 헤더로 이동 후 레이아웃에 남아있던 DarkMode 컴포넌트 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
                     {children}
                 </main>
                 <ScrollToTopButton />
-                <DarkMode />
                 <Footer />
             </body>
         </html>
